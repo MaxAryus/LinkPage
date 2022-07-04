c74e9e0199c6be801ff1ea4a66f24f4535e247ca
Max Aryus <maxaryus@aryussoftwareapplications.com>
2022-07-04 18:57:27 +0200

    changed shop link

--- src/homescreen/homescreen.jsx
+++ src/homescreen/homescreen.jsx
@@ -19,7 +19,8 @@ const Homescreen = () => {
   const appStoreCardText =
     "Du hast eine App entwickelt und möchtet diese veröffentlichen? Dann musst du nur noch Bilder für den App Store erstellen. Um diese optisch von anderen App Store Bilder abzuheben, haben wir 5 Templates für dich erstellt.";
   const appStoreCardLinkText = "Jetzt Starten";
-  const appStoreCardLink = "https://maxaryus.gumroad.com/l/eddvp";
+  const appStoreCardLink =
+    "https://maxaryus.gumroad.com/l/appstore-vorschau-bilder";
 
   return (
     <div className="container-fluid p-0 m-0">
